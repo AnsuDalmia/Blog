c249a519ac252083ebcd8d2701e09fd8fcfbe8ad
AnsuDalmia <ansu13aeccse@gmail.com>
2021-08-13 13:42:56 +0530

    first7

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,18 @@
-import { Jumbotron,Container } from "react-bootstrap";
+import { Jumbotron, Container} from 'react-bootstrap';
 import "./header.css";
-
 
 export default function Header() {
   return (
-    <Jumbotron fluid className="jumbotron">
-      <Container fluid>
+    <Jumbotron fluid>
+       <Container fluid>
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm"></span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      
     </div>
     </Container>
     </Jumbotron>
+
   );
 }
