0c86298ec93f1b794c3f65eebb0ccbade047c79f
AnsuDalmia <ansu13aeccse@gmail.com>
2021-08-13 13:36:25 +0530

    first7

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,11 +11,7 @@ export default function Header() {
         <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
       </div>
-      <img
-        className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        alt=""
-      />
+      
     </div>
     </Container>
     </Jumbotron>
